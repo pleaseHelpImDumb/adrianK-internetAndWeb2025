b2354d9ae9f409a06f58c5c27ee65b2eecb43f52
pleaseHelpImDumb <31854027+pleaseHelpImDumb@users.noreply.github.com>
2025-06-17 18:03:04 -0400

    HW3 Q1-Q4 done

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const selectNav = document.querySelector("select");
 const hw1 = document.querySelector("#hw1-section");
 const hw2 = document.querySelector("#hw2-section");
+const hw3 = document.querySelector("#hw3-section");
 
 function hideAll(){
     hw1.style.display = "none";
@@ -18,5 +19,8 @@ selectNav.addEventListener('change', function() {
     }
     else if(selected == "HW2"){
         hw2.style.display = "block";
+    }
+    else if(selected == "HW3"){
+        hw3.style.display = "block";
     }
 });
